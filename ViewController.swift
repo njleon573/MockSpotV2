f0a6f12bc82b10aa77bf13652caf220128b7f685
Nick John <Nick@Nicks-MacBook-Pro-2.local>
2019-03-04 08:29:33 -0800

    first commit1

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockSpotV2
+//
+//  Created by Nick John on 3/3/19.
+//  Copyright © 2019 Nick John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
